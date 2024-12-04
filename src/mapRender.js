8c997ecba4fd957c7327ad7e09bcf5bed10b8aef
Central3 <pawarrsumukha@gmail.com>
2024-12-04 12:34:45 +0530

    change map tile layer

--- src/mapRender.js
+++ src/mapRender.js
@@ -12,16 +12,12 @@ function renderMap(parentElement, lat, lng) {
     scrollWheelZoom: false,
   }).setView([lat, lng], 13);
 
-  L.tileLayer(
-    "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}",
-    {
-      minZoom: 10,
-      maxZoom: 12,
-      attribution:
-        '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      ext: "png",
-    },
-  ).addTo(map);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    minZoom: 10,
+    maxZoom: 12,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
 }
 
 export default renderMap;
